f0fca789ec21e874e78245f2d84f288b887d7a2e
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 04:28:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
-function Point (x, y) {
-  this.x = x
-  this.y = y
-
-  Point.prototype.toString = function () {
-    return "(" + this.x + ", " + this.y + ")"
-
-
-  }
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
 }
 
-function Side (length) {
-  this.length = length
+Point.prototype.toString = function() {
+  return("(" + this.x + "," + this.y + ")");
 }
 
-function Shape () {
+function Side(length) {
+  this.length = length;
+}
 
+function Shape() {}
 
-
-
-
-  Shape.prototype.addToPlane = function (x,y) {
-    this.position = new Point(x,y)
-  }
-
-  Shape.prototype.move = function (x,y) {
-    this.position = new Point(x,y)
-
-  }
+Shape.prototype.addToPlane = function(x, y) {
+  this.position = new Point(x,y);
+}
+Shape.prototype.move = function(x,y) {
+  this.position = new Point(x,y);
 }
