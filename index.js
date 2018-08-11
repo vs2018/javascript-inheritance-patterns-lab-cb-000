14f383dbbb9987282b567d7afb64d0203e536b42
Vishal Sharma <vsh1819@gmail.com>
2018-08-11 04:23:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function Shape () {
 
 
   Shape.prototype.addToPlane = function (x, y) {
-    this.position = Point(x,y)
+    this.position = new Point(x,y)
   }
 
   Shape.prototype.move = function (x,y) {
-    this.position = Point(x,y)
+    this.position = new Point(x,y)
 
   }
 }
